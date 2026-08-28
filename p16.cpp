0aa85e9bcf28d47cc553402df250c223a29fff97
Sanith R Shetty <sanithrshetty15@gmail.com>
2026-08-28 21:17:51 +0530

    Day-4

---
+++ p16.cpp
@@ -0,0 +1,21 @@
+// program to check whether a given number is a prime number or not
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    long num;
+
+    cout<<"Enter the number: ";
+    cin>>num;
+
+    for(int i=2;i<=num/2;++i){
+        if(num%i==0){
+            cout<<num<<" is not Prime number."<<endl;
+            exit(0);
+        }
+    }
+    cout<<num<<" is a Prime number."<<endl;
+
+    return 0;
+}
