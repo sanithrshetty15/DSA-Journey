a78342a3c25605fa95eeb8a0bde61888f25b81c8
Sanith R Shetty <sanithrshetty15@gmail.com>
2026-09-10 20:26:55 +0530

    Pattern

---
+++ p34.cpp
@@ -0,0 +1,26 @@
+//Floyd's Triangle pattern
+/*
+Enter the number: 4
+1  
+2  3  
+4  5  6  
+7  8  9  10  
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout<<"Enter the number: ";
+    cin>>num;
+    int k=1;
+    for(int i=0;i<num;i++){
+        for (int j=0;j<i+1;j++){
+            cout<<k<<"  ";
+            k++;
+        }
+        cout<<endl;
+    }
+}
