06f3ab761024f4a7b63ce858df922f67475b0d6c
Sanith R Shetty <sanithrshetty15@gmail.com>
2026-09-10 18:02:40 +0530

    pattern

---
+++ p29.cpp
@@ -0,0 +1,20 @@
+//Pattern
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num=1,n;
+
+    cout<<"Enter the number:";
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cout<<num<<"\t";
+            num++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
